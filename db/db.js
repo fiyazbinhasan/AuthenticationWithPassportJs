4ab798707622b97673ece4b3cc9312824150872c
fiyazbinhasan <fiyazhasan@live.com>
2014-10-12 21:03:33 +0600

    first commit

---
+++ db/db.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Fiyaz on 10/11/2014.
+ */
+module.exports = {
+    'url' : 'mongodb://fiyazhasan:passport@ds039850.mongolab.com:39850/passport'
+}
+
+
+/* MongoLab Instance
+mongodb://<dbuser>:<dbpassword>@novus.modulusmongo.net:27017/<dbName>
+mongodb://fiyazhasan:passport@ds039850.mongolab.com:39850/passport
+ */
---
+++ routes/routes.js
@@ -0,0 +1,49 @@
+/**
+ * Created by Fiyaz on 10/11/2014.
+ */
+module.exports = function(app, passport) {
+
+    app.get('/', function(req, res) {
+        res.render('index', { title: 'Express' });
+    });
+
+    app.get('/login', function(req, res) {
+        res.render('login', { message: req.flash('loginMessage') });
+    });
+
+    app.get('/signup', function(req, res) {
+
+        res.render('signup', { message: req.flash('signupMessage') });
+    });
+
+    app.get('/users', isLoggedIn, function(req, res) {
+        res.render('users', {
+            user : req.user,
+            title: 'NerdCats',
+            paragraph: 'Behold!!!! Your own NerdGames Server'
+        });
+    });
+
+    app.get('/logout', function(req, res) {
+        req.logout();
+        res.redirect('/');
+    });
+
+    app.post('/signup', passport.authenticate('local-signup', {
+        successRedirect : '/users',
+        failureRedirect : '/signup',
+        failureFlash : true
+    }));
+
+    app.post('/login', passport.authenticate('local-login', {
+        successRedirect : '/users',
+        failureRedirect : '/login',
+        failureFlash : true
+    }));
+};
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated())
+        return next();
+    res.redirect('/');
+}
